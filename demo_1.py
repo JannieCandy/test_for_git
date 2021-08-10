4bb20b5bbe7314898619b6b3b3c6588a8b8b5e8a
baji <gigibbyyc@163.com>
2021-08-10 16:27:20 +0800

    first commit

---
+++ demo_1.py
@@ -0,0 +1,16 @@
+#!usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author: bajijiang
+@file: demo_1.py
+@time: 2021/08/10
+@desc:
+"""
+
+print('demo,git!1')
+print('demo,git!2')
+print('demo,git!3')
+print('demo,git!4')
+print('demo,git!5')
+print('demo,git!6')
+print('demo,git!7')
